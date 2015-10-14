e51d4b3421095d71452302b2d6c168ae782ac77f
ifiliuk <ifiliuk@gorillagroup.com>
2015-10-14 20:45:12 +0300

    added:  jquery, band.json, routing for albums

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 angular
-    .module("albumsModule",[])
+    .module("okeanModule",["ngRoute"])
+    .config(function($routeProvider){
+        $routeProvider
+            .when('/album/:index',{
+                templateUrl: '/album-detail.html',
+                controller: 'albumDetail'
+            })
+            .when('/', {
+                templateUrl: '/album-list.html'
+            })
+    })
+    .controller("bandController", function($scope, $http){
+        $scope.band = [];
+
+        $http.get('/band.json').
+            success(function(data, status, headers, config) {
+                for(var i=0; i<data["band"].length; i++){
+                    $scope.band.push(data["band"][i]);
+                }
+            }).
+            error(function(data, status, headers, config) {
+                alert('error');
+            });
+    })
     .controller("albumsController", function($scope, $http){
         $scope.albums = [];
 
@@ -13,4 +36,8 @@ angular
                 alert('error');
             });
 
+    })
+    .controller("albumDetail", function($scope, $routeParams){
+        $scope.album = $scope.albums[$routeParams.index];
+        $scope.songs = $scope.album["songs"];
     });
